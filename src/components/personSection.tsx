1946af4f258e6c3635291563fa634d5564a9e67a
Lex von Biedenfeld <avonbied@users.noreply.github.com>
2021-03-04 14:11:34 -0600

    Update title type signature in PersonSection

--- src/components/personSection.tsx
+++ src/components/personSection.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import {Person, PersonCard} from './personCard';
 import * as styles from '../styles/components/personSection.module.scss';
 
-const PersonSection = ({people, title}: {people: Person[], title: string | Node}) => {
+const PersonSection = ({people, title}: {people: Person[], title: string | React.ReactNode}) => {
     return (
         <>
         <h2 className={styles.sectionTitle}>{title}</h2>
-        <section className={styles.personSection}>{people.map((person: Person) => <PersonCard {...person} />)}</section>
+        <section className={styles.personSection}>{people.map((person, index) => <PersonCard key={index} {...person} />)}</section>
         </>
     );
 }
